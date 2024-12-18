2e66a15f236591983a0da61248f99801259545a3
Moonbaboon3 <147054617+Moonbaboon3@users.noreply.github.com>
2024-12-18 18:42:01 +0200

    Add files via upload

---
+++ src/main/java/com/example/lms/Repositories/EnrollmentC1Repo.java
@@ -0,0 +1,9 @@
+package com.example.lms.Repositories;
+
+import com.example.lms.entities.Enrollment;
+
+import java.util.List;
+
+public interface EnrollmentC1Repo {
+    List<Enrollment>enrollments();
+}
